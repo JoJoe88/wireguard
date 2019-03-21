c524bb8591a96bc3b89380dd2822c0dbc800d416
JoJoe88 <jsun88@yeah.net>
2019-03-21 14:30:57 +0800

    Create gcp-debian-wg.sh
    Debian 多用户wireguard脚本

---
+++ gcp-debian-wg.sh
@@ -0,0 +1,117 @@
+# 高速新VPN协议WireGuard服务端一键脚本
+# GCP香港
+
+# 定义常量
+let port=9009
+mtu=1460
+ip_list=(4 5 8 178 186 118 158 198 168 9)
+ipv6_range="fd08:620c:4df0:65eb::"
+serverip=$(curl -4 ip.sb)
+
+
+# 安装WireGuard和辅助库 resolvconf
+echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
+printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
+apt update
+apt install wireguard resolvconf -y
+
+# 安装二维码插件
+if [ ! -f '/usr/bin/qrencode' ]; then
+    apt -y install qrencode
+fi
+
+# 打开ip4/ipv6防火墙转发功能
+sysctl_config() {
+    sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+    sed -i '/net.ipv6.conf.all.forwarding/d' /etc/sysctl.conf
+    sed -i '/net.ipv6.conf.default.accept_ra/d' /etc/sysctl.conf
+    echo 1 > /proc/sys/net/ipv4/ip_forward
+    echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.forwarding = 1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.default.accept_ra=2" >> /etc/sysctl.conf
+    sysctl -p >/dev/null 2>&1
+}
+sysctl_config
+
+# 配置WireGuard文件目录 /etc/wireguard
+mkdir -p /etc/wireguard
+chmod 777 -R /etc/wireguard
+cd /etc/wireguard
+
+# 生成 密匙对(公匙+私匙)
+wg genkey | tee sprivatekey | wg pubkey > spublickey
+wg genkey | tee cprivatekey | wg pubkey > cpublickey
+
+# 生成服务端配置文件
+cat <<EOF >wg0.conf
+[Interface]
+PrivateKey = $(cat sprivatekey)
+Address = 10.0.0.1/24,  ${ipv6_range}1/64
+PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE; ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+ListenPort = $port
+DNS = 8.8.8.8, 2001:4860:4860::8888
+MTU = $mtu
+
+[Peer]
+PublicKey = $(cat cpublickey)
+AllowedIPs = 10.0.0.2/32,  ${ipv6_range}188
+
+EOF
+
+
+# 生成客户端配置
+cat <<EOF >client.conf
+[Interface]
+PrivateKey = $(cat cprivatekey)
+Address = 10.0.0.2/24,  ${ipv6_range}188/64
+DNS = 8.8.8.8, 2001:4860:4860::8888
+#  MTU = $mtu
+#  PreUp =  start   .\route\routes-up.bat
+#  PostDown = start  .\route\routes-down.bat
+
+[Peer]
+PublicKey = $(cat spublickey)
+Endpoint = $serverip:$port
+AllowedIPs = 0.0.0.0/0, ::0/0
+PersistentKeepalive = 25
+
+EOF
+
+
+# 添加 2-9 号多用户配置
+for i in {2..9}
+do
+    ip=10.0.0.${ip_list[$i]}
+    ip6=${ipv6_range}${ip_list[$i]}
+    wg genkey | tee cprivatekey | wg pubkey > cpublickey
+
+    cat <<EOF >>wg0.conf
+[Peer]
+PublicKey = $(cat cpublickey)
+AllowedIPs = $ip/32, $ip6
+
+EOF
+
+    cat <<EOF >wg_client_$i.conf
+[Interface]
+PrivateKey = $(cat cprivatekey)
+Address = $ip/24, $ip6/64
+DNS = 8.8.8.8, 2001:4860:4860::8888
+
+[Peer]
+PublicKey = $(cat spublickey)
+Endpoint = $serverip:$port
+AllowedIPs = 0.0.0.0/0, ::0/0
+PersistentKeepalive = 25
+
+EOF
+    cat /etc/wireguard/wg_client_$i.conf | qrencode -o wg_client_$i.png
+done
+
+
+# 启动WireGuard
+wg-quick up wg0
+
+# 设置开机启动
+systemctl enable wg-quick@wg0
